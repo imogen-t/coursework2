d5ab2b54e9b0607caa836f804b538e5c4cea623a
imogen-t <irt1g19@soton.ac.uk>
2022-05-22 13:40:04 +0100

    Standardise data to normal distribution

--- logistic-regression.py
+++ logistic-regression.py
@@ -1,26 +1,30 @@
 # %%
 import numpy as np 
 import pandas as pd 
+import random
 
 from sklearn import preprocessing
 
 # %%
 # Import training data and split to explainatory variables and outcomes
-# Standardise the training explanatory variables
 
 df = pd.read_csv('TrainingData.txt', header=None)
 
 X_train = df.iloc[:,:24]
 y_train = df.iloc[:,24:]
 
-
 X_train = preprocessing.scale(X_train)
+
+X_test = pd.read_csv('TestingData.txt', header=None)
+
+X_test = preprocessing.scale(X_test)
+
 
 # %%
 # Sigmoid
 def sigmoid(z):
     out = 1/(1+np.exp(-z))
-    return out
+    return out >= 1/2
 
 # %%
 # Optimise
@@ -33,14 +37,14 @@ def optimise(x, y,parameters,learning_rate=0.02,iterations=500):
     for i in range(iterations): 
         sigma = sigmoid(np.dot(x, weight) + bias)
         loss = -1/size * np.sum(y * np.log(sigma)) + (1 - y) * np.log(1-sigma)
-    #     dW = 1/size * np.dot(x.T, (sigma - y))
-    #     db = 1/size * np.sum(sigma - y)
-    #     weight -= learning_rate * dW
-    #     bias -= learning_rate * db 
+        dW = 1/size * np.dot(x.T, (sigma - y))
+        db = 1/size * np.sum(sigma - y)
+        weight -= learning_rate * dW
+        bias -= learning_rate * db 
     
-    # parameters["weight"] = weight
-    # parameters["bias"] = bias
-    # return parameters
+    parameters["weight"] = weight
+    parameters["bias"] = bias
+    return parameters
 
 # %%
 # Initialise weight and bias to 0 values
@@ -48,12 +52,19 @@ def optimise(x, y,parameters,learning_rate=0.02,iterations=500):
 
 init_parameters = {}
 init_parameters["weight"] = np.zeros(X_train.shape[1])
-init_parameters["bias"] = 0
+init_parameters["bias"] = random.uniform(-0.1,0.1)
 
 
 # %% 
 # Train the model
 
-parameters_out = optimise(X_train, y_train, parameters=init_parameters)
+
+training = optimise(X_train, y_train[24], parameters=init_parameters)
+
+y_train_hat = np.dot(X_test,training["weight"]+training["bias"])
+
+predictions = sigmoid(y_train_hat)
+
+
 
 # %%
