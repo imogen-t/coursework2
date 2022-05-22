faa8dff61ca30648827d29e2090d57eaea4b63f7
imogen-t <irt1g19@soton.ac.uk>
2022-05-22 14:52:25 +0100

    TODO: Plot graphs to show sep

--- logistic-regression.py
+++ logistic-regression.py
@@ -1,9 +1,8 @@
 # %%
 import numpy as np 
 import pandas as pd 
-import random
-
 from sklearn import preprocessing
+
 
 # %%
 # Import training data and split to explainatory variables and outcomes
@@ -13,7 +12,9 @@ df = pd.read_csv('TrainingData.txt', header=None)
 X_train = df.iloc[:,:24]
 y_train = df.iloc[:,24:]
 
-X_train = preprocessing.scale(X_train)
+# TODO: Plot graphs of data
+
+X_train = preprocessing.scale(X_train.x, X_train.y)
 
 X_test = pd.read_csv('TestingData.txt', header=None)
 
@@ -27,13 +28,13 @@ def sigmoid(z):
     return out >= 1/2
 
 # %%
-# Optimise
-def optimise(x, y,parameters,learning_rate=0.02,iterations=500): 
+# Learning
+def optimise(x, y,parameters,learning_rate=0.02,iterations=1000): 
     size = x.shape[0]
     weight = parameters["weight"] 
     bias = parameters["bias"]
 
-
+    # iterate over data to aim for convergence
     for i in range(iterations): 
         sigma = sigmoid(np.dot(x, weight) + bias)
         loss = -1/size * np.sum(y * np.log(sigma)) + (1 - y) * np.log(1-sigma)
@@ -59,15 +60,17 @@ init_parameters["bias"] = 0
 # %% 
 # Train the model
 
-
 training = optimise(X_train, y_train[24], parameters=init_parameters)
 
 
 X_testdf = pd.DataFrame(X_test)
-predictions = pd.DataFrame(sigmoid(np.dot(X_test,training["weight"]+training["bias"])))
+out = np.dot(X_test,training["weight"]+training["bias"])
+predictions = pd.DataFrame(sigmoid(out))
 
 X_testdf['prediction'] = predictions
 
 abnormals = X_testdf.loc[X_testdf['prediction']]
+
+
 
 # %%
