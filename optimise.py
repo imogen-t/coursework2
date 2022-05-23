07708981b46a641484960442c5f217ccd0c6398c
imogen-t <irt1g19@soton.ac.uk>
2022-05-23 21:43:41 +0100

    Update for graphing

--- optimise.py
+++ optimise.py
@@ -1,5 +1,6 @@
 # %%
 import csv
+import matplotlib.pyplot as plt
 
 # %%
 # Import data
@@ -84,28 +85,49 @@ def optimiseTimes_1curve(priceCurve, user):
         optimal.append(opTimes)
     return optimal
 
-def optimiseTimes_allCurves(curves, user, index):
+def optimiseTimes_allCurvesAndUsers(curves, users):
     optimal = []
     for curve in curves:
-        o = optimiseTimes_1curve(curve, user)
-        optimal.append(o)
-    filename = "user" + str(index) + "-optimalTimes.csv"
-    write_csv(filename, optimal)
+        for user in users:
+            o = optimiseTimes_1curve(curve, user)
+            optimal.append(o)
     return optimal
 
-user1_optimised = optimiseTimes_allCurves(priceCurves, user1,1)
-user2_optimised = optimiseTimes_allCurves(priceCurves, user2,2)
-user3_optimised = optimiseTimes_allCurves(priceCurves, user3,3)
-user4_optimised = optimiseTimes_allCurves(priceCurves, user4,4)
-user5_optimised = optimiseTimes_allCurves(priceCurves, user5,5)
-# %%
-# write outputs to csv
+allOptimised = optimiseTimes_allCurvesAndUsers(priceCurves, users)
 
-def write_csv(filename, data):
-    file = open(filename, 'w', newline ='') 
-    with file:     
-        write = csv.writer(file) 
-        write.writerows(data) 
+# lists of users split by device times under each price curve
+optimisedA = allOptimised[::3]
+optimisedB = allOptimised[1::3]
+optimisedC = allOptimised[2::3]
+# %%
+# get frequency counts for each time
+concated = []
+for elem in optimisedA:
+    for e in elem:
+        concated += e
+
+counts = list({(x,concated.count(x)) for x in concated})
+
+# fill in emptys
+for i in range(24):
+    if([item for item in counts if item[0] == i] == []):
+        counts.append((i,0))
+
+counts = sorted(counts, key= lambda x : x[0])
+
+# %%
+# plot bar chart for each time
+
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+times = list(zip(*counts))[0]
+frequencies = list(zip(*counts))[1]
+ax.bar(times, frequencies)
+plt.title("Optimal Cumulative Energy Use by 5 Users with IoT Devices by Hour")
+plt.xlabel("Time, in 24-hour format")
+plt.ylabel("Number of IoT devices used in this hour")
+plt.xticks(range(24))
+plt.show()
 
 
 # %%
